72b2c5be016552bc51aeb106eeee660b3c93e284
Stanko <stanko.tadic@gmail.com>
2018-12-26 13:11:51 +0100

    Readme updates

---
+++ src/generator/index.ts
@@ -0,0 +1,9 @@
+import generateAsyncReducer from './generate-async-reducer';
+import Reducer from './reducer';
+import spark from './spark';
+
+export default spark;
+export {
+  Reducer,
+  generateAsyncReducer,
+};
