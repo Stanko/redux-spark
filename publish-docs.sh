98f11be2be0ceb1d4e056a780011c7c4fe240613
Stanko <stanko.tadic@gmail.com>
2018-12-26 13:50:42 +0100

    Publish docs script

---
+++ publish-docs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Build docs
+npm run build
+# Switch to gh-pages branch
+git checkout gh-pages
+# Copy everything from build to root
+cp ./build/* ./
+# Commit with current time
+git add .
+# Commit with current time
+git commit -a -m "Demo updated `date +'%Y-%m-%d %H:%M:%S'`"
+# Pull changes, if any
+git pull --rebase origin gh-pages
+# Push changes
+git push origin gh-pages
+# Switch back to the branch
+git checkout -
